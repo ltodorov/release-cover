9694e05b78ffb3799ac79b97808c5dae03274a5e
Lyubomir Todorov <lyubomir.todorov@gmail.com>
2021-02-02 22:09:33 +0200

    Add favicon

--- webpack.common.js
+++ webpack.common.js
@@ -30,7 +30,8 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
+            template: "./src/index.html",
+            favicon: "./src/favicon.ico"
         })
     ]
 };
