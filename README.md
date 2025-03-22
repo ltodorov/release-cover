5c3a8114be06434d0e0f0ec1e76003671ddbe43a
Lyubomir Todorov <lyubomir.todorov@gmail.com>
2025-03-22 20:14:18 +0200

    Replace Biome with ESLint and Prettier

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A web page that generates release covers for [No Smoking Recordings](https://www
 
 [![Node.js CI](https://github.com/ltodorov/release-cover/actions/workflows/node.js.yml/badge.svg?branch=main)](https://github.com/ltodorov/release-cover/actions/workflows/node.js.yml)
 
-*"Release Cover" by Lyubomir Todorov is licensed under CC BY-NC-ND 4.0*.
+_"Release Cover" by Lyubomir Todorov is licensed under CC BY-NC-ND 4.0_.
--- src/helpers/get-image.ts
+++ src/helpers/get-image.ts
@@ -1,9 +1,9 @@
-async function getImage(src: string): Promise<HTMLImageElement> {
+function getImage(src: string): Promise<HTMLImageElement> {
     return new Promise<HTMLImageElement>((resolve, reject) => {
         const error = `An error from getImage(${src})`
         const img = new Image()
         img.addEventListener("load", () => resolve(img))
-        img.addEventListener("error", err => reject(err.message ?? error))
+        img.addEventListener("error", (err) => reject(err.message ?? error))
         img.src = src
     })
 }
